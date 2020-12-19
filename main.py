3617d7b8790db1c321386a4dbe9c8824b1091d08
Aurélien PUJOL <43996081+AurelPuj@users.noreply.github.com>
2020-12-19 14:25:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,32 @@ import scipy.io
 import pandas as pd
 import matplotlib.pyplot as plt
 file_name = "..\\DataBase\\SEED-VIG\\Raw_Data\\1_20151124_noon_2.mat"
+
+#On récupère les données stockées dans le fichier mat
 mat = scipy.io.loadmat(file_name) 
 
+#On extrait les data de chaque éléctrodes 
 raw_data = mat['EEG'][0][0][0] # Data of each band
 
+#On crée un tableau afin de stocker les différents étiquettes des canaux de l'EEG
 band_headers = []
 
 for band in mat['EEG'][0][0][1][0] :
     band_headers.append(band[0])
-    
+
+#On crée un dictoinnaire afin d'associer chaque valeur à chaque séquence de données mesurées
 data_dict={}
 
+#On initialise chaque cannaux avec un tableau afin de mesurée celui-ci
 for i in range (0,len(band_headers)):
     data_dict[band_headers[i]] = []
 
+#On rempli les tableau avec les données
 for sample in raw_data:
     for i in range(0,sample.size):
         data_dict[band_headers[i]].append(sample[i])
 
+#On affiche chaque cannal sur un subplot
 fig,axs = plt.subplots(6,3)
 row = 0
 color=['red','darkblue','orange','purple','darkgreen','brown','lightgreen','lightblue','magenta','orangered','salmon','blue']
@@ -37,7 +45,6 @@ for column,band in enumerate(data_dict) :
 border=[0.05,0.95,0.6,0.2]
 fig.subplots_adjust(border[0],border[0],border[1],border[1],border[-1],border[-2])
 plt.show()
-
 
 
 import os
