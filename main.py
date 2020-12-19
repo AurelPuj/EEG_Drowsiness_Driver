32f8a71b7d0d40c23e3ffce0fa0e9882684b467b
Aurélien PUJOL <43996081+AurelPuj@users.noreply.github.com>
2020-12-19 23:26:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,60 @@
 
 Ceci est un script temporaire.
 """
-
+import os
 import scipy.io 
 import pandas as pd
 import matplotlib.pyplot as plt
-file_name = "..\\DataBase\\SEED-VIG\\Raw_Data\\1_20151124_noon_2.mat"
+Path = "..\\DataBase\\SEED-VIG\\Raw_Data"
+Path_csv = "..\\DataBase\\SEED-VIG\\EEG_csv"
 
-#On récupère les données stockées dans le fichier mat
-mat = scipy.io.loadmat(file_name) 
+listeFichiers = []
+for (repertoire, sousRepertoires, fichiers) in os.walk(Path):
+    listeFichiers.extend(fichiers)
+    
+listeCsv = []
+for (repertoire, sousRepertoires, fichiers) in os.walk(Path_csv):
+    listeCsv.extend(fichiers)
 
-#On extrait les data de chaque éléctrodes 
-raw_data = mat['EEG'][0][0][0] # Data of each band
 
-#On crée un tableau afin de stocker les différents étiquettes des canaux de l'EEG
-band_headers = []
+for file_name in listeFichiers :
+    file_csv = file_name.replace(".mat",".csv")
+    csv_path = Path_csv+"\\" + file_csv
+    
+    if file_csv not in listeCsv :
+        #On récupère les données stockées dans le fichier mat
+        data_mat = scipy.io.loadmat(Path + "\\" + file_name)
+        
+        #On extrait les data de chaque éléctrodes 
+        raw_data = data_mat['EEG'][0][0][0] # Data of each band
+        
+        #On crée un tableau afin de stocker les différents étiquettes des canaux de l'EEG
+        band_headers = []
+        
+        for band in data_mat['EEG'][0][0][1][0] :
+            band_headers.append(band[0])
+        
+        #On crée un dictoinnaire afin d'associer chaque valeur à chaque séquence de données mesurées
+        data_dict={}
+        
+        #On initialise chaque cannaux avec un tableau afin de mesurée celui-ci
+        for i in range (0,len(band_headers)):
+            data_dict[band_headers[i]] = []
+        
+        #On rempli les tableau avec les données
+        for sample in raw_data:
+            for i in range(0,sample.size):
+                data_dict[band_headers[i]].append(sample[i])
+        
+        #On crée un dataframe dans lequel on stock le dictionnaire -> plus rapide que de crée le data frame direcctment
+        df = pd.DataFrame(data_dict)
+        df.to_csv(csv_path, sep=";", index=False)
+        print(df)
+    else :
+        df = pd.read_csv(csv_path, sep=";")
+        print(df)
 
-for band in mat['EEG'][0][0][1][0] :
-    band_headers.append(band[0])
-
-#On crée un dictoinnaire afin d'associer chaque valeur à chaque séquence de données mesurées
-data_dict={}
-
-#On initialise chaque cannaux avec un tableau afin de mesurée celui-ci
-for i in range (0,len(band_headers)):
-    data_dict[band_headers[i]] = []
-
-#On rempli les tableau avec les données
-for sample in raw_data:
-    for i in range(0,sample.size):
-        data_dict[band_headers[i]].append(sample[i])
-
+'''
 #On affiche chaque cannal sur un subplot
 fig,axs = plt.subplots(6,3)
 row = 0
@@ -45,7 +69,7 @@ for column,band in enumerate(data_dict) :
 border=[0.05,0.95,0.6,0.2]
 fig.subplots_adjust(border[0],border[0],border[1],border[1],border[-1],border[-2])
 plt.show()
+'''
 
-
-import os
 os.system("pause")
+
